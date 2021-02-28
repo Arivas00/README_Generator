d31adb9f8ca1e19a3c921f6e86129cbd586bd5bd
Arivas <33260080+Arivas00@users.noreply.github.com>
2021-02-27 20:47:58 -0500

    Added all inquirer questions

--- index.js
+++ index.js
@@ -5,26 +5,55 @@ inquirer
     .prompt([
         {
             type: "input",
-            message: "What is your title?",
+            message: "What is your GitHub username?",
+            name: "username",
+        },
+        {
+            type: "input",
+            message: "What is your email address?",
+            name: "email",
+        },
+        {
+            type: "input",
+            message: "What is your project's title?",
             name: "title",
         },
         {
             type: "input",
-            message: "Give a description.",
+            message: "Give a short description of your project.",
             name: "description",
         },
         {
-            type: "input",
-            message: "How is it installed",
-            name: "installation",
+            type: "list",
+            message: "What kind of license should your project have?",
+            name: "license",
+            choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
         },
         {
             type: "input",
-            message: ""
-        }
+            message: "What command should be run to install dependencies?",
+            name: "installation",
+            default: "npm i",
+        },
+        {
+            type: "input",
+            message: "What command should be run to run tests?",
+            name: "usage",
+            default: "npm test",
+        },
+        {
+            type: "input",
+            message: "What does the user need to know about using the repo?",
+            name: "use",
+        },
+        {
+            type: "input",
+            message: "What does the user need to know about contributing to the repo?",
+            name: "contribute",
+        },
     ])
     .then((response) => {
         fs.writeFile("README.md", JSON.stringify(response), (err) =>
-        err ? console.error(err) : console.log("Success!")
+        err ? console.error(err) : console.log("Generating README...")
         )
     });
